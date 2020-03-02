e61fd736291c3ee5485e665166fe23d53a2f78dc
johnpooler <johnpooler98@gmail.com>
2020-03-02 17:38:39 +0000

    exercise 1 done

--- src/main/scala/day1/basics/addAndMultiply.scala
+++ src/main/scala/day1/basics/addAndMultiply.scala
@@ -3,11 +3,12 @@ package day1.basics
 object addAndMultiply extends App{
 
 def calculation(A:Int, B:Int,C:Int):Int= {
+var nums=List(A,B,C).sorted
+  (nums(0)+nums(1))*nums(2)
 
-  A+B+C
 
 }
-  calculation(5,6,7)
+  println(calculation(5,6,7))
 }
 
 
